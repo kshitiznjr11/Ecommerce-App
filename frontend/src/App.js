771e2057c29cd3c4fb423b2f60c56f332c790e27
kshitiznjr11 <kshitiznjr1@gmail.com>
2025-06-19 13:05:25 +0545

    HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
 import React from "react";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
-  return <h1>Welcome To Proshop</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
