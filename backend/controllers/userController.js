20872e8576406269370ca696f7bbb9fd797e9638
kshitiznjr11 <kshitiznjr1@gmail.com>
2025-06-29 13:42:11 +0545

    Backend Authentication

---
+++ backend/controllers/userController.js
@@ -0,0 +1,77 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel.js";
+
+// @desc FAuth user & get token
+// @route GET /api/users/login
+// @access Public
+const authUser = asyncHandler(async (req, res) => {
+  res.send("auth user");
+});
+
+// @desc Register a new user
+// @route POST /api/users/login
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.send("register user");
+});
+
+// @desc Logout user / clear cookie
+// @route POST /api/users/logout
+// @access Private
+const logoutUSer = asyncHandler(async (req, res) => {
+  res.send("logout user");
+});
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send("get user profile");
+});
+
+// @desc Update user profile
+// @route PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send("update user profile");
+});
+
+// @desc Get user
+// @route GET /api/users
+// @access Private/admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("get users");
+});
+
+// @desc Get user by ID
+// @route GET /api/users/:id
+// @access Private/admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.send("get user by ID");
+});
+
+// @desc Delete user
+// @route DELETE /api/users
+// @access Private/admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("delete user");
+});
+
+// @desc Update user
+// @route PUT /api/users/:id
+// @access Private/admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("update user");
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUSer,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+};
--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { addToCart } from "../slices/cartSlice";
+import { addToCart, removeFromCart } from "../slices/cartSlice";
 import {
   Row,
   Col,
@@ -23,6 +23,13 @@ const CartScreen = () => {
 
   const addToCartHandler = async (product, qty) => {
     dispatch(addToCart({ ...product, qty }));
+  };
+  const removeFromCartHandler = async (id) => {
+    dispatch(removeFromCart(id));
+  };
+
+  const checkoutHandler = () => {
+    navigate("/login?redirect=/shipping");
   };
 
   return (
@@ -61,7 +68,11 @@ const CartScreen = () => {
                     </Form.Control>
                   </Col>
                   <Col md={2}>
-                    <Button type="button" variant="light">
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item._id)}
+                    >
                       <FaTrash />
                     </Button>
                   </Col>
@@ -89,6 +100,7 @@ const CartScreen = () => {
                 type="button"
                 className="btn-block"
                 disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
               >
                 Proceed To Checkout
               </Button>
