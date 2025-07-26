98ed66f827a436b7b2bdddd0998ea063c014a041
kshitiznjr11 <kshitiznjr1@gmail.com>
2025-07-26 15:34:54 +0545

    RESTART

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,12 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import './App.css';
+import Navbar from './components/navbar/Navbar';
 
-const App = () => {
+function App() {
   return (
-    <>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <Outlet />
-        </Container>
-      </main>
-      <Footer />
-      <ToastContainer />
-    </>
+    <div>
+      <Navbar />
+    </div>
   );
-};
+}
 
 export default App;
